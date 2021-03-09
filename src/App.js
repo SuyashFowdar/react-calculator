ff6d1ee57a0d51422af5f77a39e6ba4c0e084d76
Suyash <suyash.krishna9@gmail>
2021-03-09 16:50:03 +0400

    Add App component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ButtonPanel from './ButtonPanel';
+
+const App = () => (
+  <div>
+    <div>Display</div>
+    <ButtonPanel />
+  </div>
+);
+
+export default App;
