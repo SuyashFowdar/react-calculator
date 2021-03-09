e3b87205168a47e41ce6e86914d56f1908b8b07b
Suyash <suyash.krishna9@gmail>
2021-03-09 20:55:41 +0400

    Add numbers and symbols

--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Button = () => (
-  <div>
-    <div>Button</div>
-  </div>
-);
+const Button = props => {
+  const { numbers, symbols } = props;
+  const numberList = numbers.map(number => <button type="button" key={number.toString()}>{number}</button>);
+  const symbolList = symbols.map(symbol => <button type="button" key={symbol.toString()}>{symbol}</button>);
+
+  return (
+    <div>
+      <div>{numberList}</div>
+      <div>{symbolList}</div>
+    </div>
+  );
+};
+
+Button.propTypes = {
+  numbers: PropTypes.number.isRequired,
+  symbols: PropTypes.string.isRequired,
+};
 
 export default Button;
--- src/ButtonPanel.js
+++ src/ButtonPanel.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Button from './Button';
 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const symbols = ['+', '-', '*', '/', '='];
+
 const ButtonPanel = () => (
   <div>
     <div>Button Panel</div>
-    <Button />
+    <Button numbers={numbers} symbols={symbols} />
   </div>
 );
 
