0d63c7fa76f1697e85cf040a7d1c941e6d62ac19
Suyash <suyash.krishna9@gmail>
2021-03-10 14:43:37 +0400

    Remove numbers and symbols and put name

---
+++ src/components/Button.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = props => {
+  const { name } = props;
+
+  return (
+    <div>
+      <button type="button">{name}</button>
+    </div>
+  );
+};
+
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+export default Button;
