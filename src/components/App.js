20daef9c69eb406a6222dc0e0183581f029616c2
Suyash <suyash.krishna9@gmail>
2021-03-13 20:25:14 +0400

    Create handleClick + resultStr functions

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,31 @@ import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import calculate from '../logic/calculate'; // eslint-disable-line
 
-const App = () => (
-  <>
-    <div className="row main-center cross-center h-100">
-      <div className="col">
-        <Display />
-        <ButtonPanel />
+const App = () => {
+  const [total, setTotal] = React.useState(null);
+  const [next, setNext] = React.useState(null);
+  const [operation, setOperation] = React.useState(null);
+  const handleClick = buttonName => {
+    const result = calculate({ total, next, operation }, buttonName);
+    setTotal(result.total);
+    setNext(result.next);
+    setOperation(result.operation);
+  };
+  const resultStr = () => {
+    const result = `${total}${operation}${next}`.replace(/null/g, '');
+    return result === '' ? undefined : result;
+  };
+
+  return (
+    <>
+      <div className="row main-center cross-center h-100">
+        <div className="col">
+          <Display result={resultStr()} />
+          <ButtonPanel handleClick={handleClick} />
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default App;
